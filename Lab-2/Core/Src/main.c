29dea3d4c2eb566669a0a1626caa1eaf0732119e
3ilib0ba <evonniy@mail.ru>
2023-10-17 00:29:34 +0300

    start lab

--- Lab-2/Core/Src/main.c
+++ Lab-2/Core/Src/main.c
@@ -23,6 +23,21 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include <string.h>
+#include <stdio.h>
+
+/*
+
+#include <stdbool.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdio.h>
+#include <stdarg.h>
+#include <usart.h>
+#include <ctype.h>
+
+*/
+
 
 /* USER CODE END Includes */
 
@@ -43,6 +58,27 @@
 /* Private variables ---------------------------------------------------------*/
 
 /* USER CODE BEGIN PV */
+uint8_t INT_ON = 1;
+uint8_t INT_OFF = 0;
+
+uint16_t current_light = 1;
+uint32_t start_time;
+uint32_t duration = 5000;
+uint32_t duration_for_red;
+uint32_t duration_for_yellow = 3000;
+uint32_t blink_duration = 500;
+uint32_t blink_count = 0;
+uint8_t button_flag = 0;
+
+uint8_t interrupts_mode = 0;
+uint8_t is_writing_now = 0;
+char read_buffer[100];
+char write_buffer[100];
+uint8_t status;
+char *cur_process_char = read_buffer;
+char *cur_read_char = read_buffer;
+char *transmit_from_pointer = write_buffer;
+char *write_to_pointer = write_buffer;
 
 /* USER CODE END PV */
 
@@ -55,105 +91,402 @@ void SystemClock_Config(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
+void wait(uint32_t duration) {
+    uint32_t startTime = HAL_GetTick();
+    while ((HAL_GetTick() - startTime) < duration) {
+    }
+}
+
+
+void turnAllOff() {
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
+}
+
+void turnSpecificLightOn(uint16_t light) {
+    turnAllOff();
+    HAL_GPIO_WritePin(GPIOD, light, GPIO_PIN_SET);
+}
+
+void blinkSpecificLight(uint32_t count, uint16_t light, uint32_t blinkDuration) {
+    for (uint32_t i = 0; i < count; i++) {
+
+        turnAllOff();
+        wait(blinkDuration);
+
+        turnSpecificLightOn(light);
+        wait(blinkDuration);
+    }
+}
+
+int check_starts_with(char *a, char *b) {
+    size_t i = 0;
+    while (a[i] != '\0') {
+        if (a[i] != b[i]) return 0;
+        i++;
+    }
+    return 1;
+}
+
+char *concat(char *s1, char *s2) {
+    char *result = malloc(strlen(s1) + strlen(s2) + 1);
+    strcpy(result, s1);
+    strcat(result, s2);
+    return result;
+}
+
+void next(char **pointer, char *buffer) {
+    if (*pointer >= buffer + 100) {
+        *pointer = buffer;
+    } else {
+        (*pointer)++;
+    }
+}
+
+void write_char_to_buff(char c) {
+    *write_to_pointer = c;
+    next(&write_to_pointer, write_buffer);
+}
+
+void write(char *str) {
+    char *str_with_newlines = concat("\r\n", str);
+    str_with_newlines = concat(str_with_newlines, "\r\n");
+    int size = sizeof(char) * strlen(str_with_newlines);
+    if (interrupts_mode == 0) {
+        HAL_UART_Transmit(&huart6, (uint8_t *) str_with_newlines, size, 10);
+    } else {
+        for (size_t i = 0; str_with_newlines[i] != '\0'; i++) {
+            write_char_to_buff(str_with_newlines[i]);
+        }
+    }
+}
+
+void write_about_info_command() {
+    char answer[100];
+    char *light;
+    switch (current_light) {
+        case GPIO_PIN_15:
+            light = "red";
+            break;
+        case GPIO_PIN_14:
+            light = "yellow";
+            break;
+        case GPIO_PIN_13:
+            light = "green";
+            break;
+        case 0:
+            light = "blinking green";
+            break;
+    }
+    uint8_t mode;
+    char interrupts;
+    if (button_flag == 0 && current_light == RED) {
+        mode = 1;
+    } else mode = 2;
+    if (interrupts_mode == 1) {
+        interrupts = 'I';
+    } else {
+        interrupts = 'P';
+    }
+
+    sprintf(answer, "Light: %s, Mode: %d, Timeout: %d, Interrupts: %c", light, mode, duration * 4, interrupts);
+    write(answer);
+}
+
+void write_command_not_found() {
+    char *str = "Invalid command. You can use: '?', set mode 1/2, set timeout X, set interrupts on/off";
+    write(str);
+}
+
+int is_number(char *str) {
+    for (size_t i = 0; str[i] != '\0'; i++) {
+        if (!isdigit(str[i])) return 0;
+    }
+    return 1;
+}
+
+void process_symbol() {
+    if (*cur_process_char == '\r') {
+        *cur_process_char = '\0';
+
+
+        char *command = strtok(read_buffer, " ");
+        if (strcmp(command, "?") == 0) {
+            write_about_info_command();
+        } else if (strcmp(command, "set") == 0) {
+            char *first_arg = strtok(NULL, " ");
+            if (strcmp(first_arg, "mode") == 0) {
+                char *mode = strtok(NULL, " ");
+                if (strcmp(mode, "1") == 0) {
+                    button_flag = 0;
+                    duration_for_red = duration * 4;
+                    write("Entered mode 1");
+                } else if (strcmp(mode, "2") == 0) {
+                    button_flag = 1;
+                    duration_for_red = duration;
+                    write("Entered mode 2");
+                } else {
+                    write_command_not_found();
+                }
+            } else if (strcmp(first_arg, "timeout") == 0) {
+                char *timeout = strtok(NULL, " ");
+                if (is_number(timeout)) {
+                    int new_dur = atoi(timeout) * 1000;
+                    if (duration_for_red == duration) {
+                        duration_for_red = new_dur / 4;
+                    } else {
+                        duration_for_red = new_dur;
+                    }
+                    write(concat("New duration is ", timeout));
+                    duration = new_dur / 4;
+                } else {
+                    write_command_not_found();
+                }
+            } else if (strcmp(first_arg, "interrupts") == 0) {
+                char *interrupts = strtok(NULL, " ");
+                if (strcmp(interrupts, "on") == 0) {
+                    interrupts_mode = 1;
+                    transmit_from_pointer = write_to_pointer;
+                    cur_read_char = read_buffer;
+                    write("Interrupt mode on");
+                    HAL_UART_Receive_IT(&huart6, (uint8_t *) cur_read_char, sizeof(char));
+                } else if (strcmp(interrupts, "off") == 0) {
+                    interrupts_mode = 0;
+                    HAL_UART_Abort_IT(&huart6);
+                    write("Interrupt mode off");
+                } else {
+                    write_command_not_found();
+                }
+            } else {
+                write_command_not_found();
+            }
+        } else {
+            write_command_not_found();
+        }
+        cur_process_char = read_buffer;
+    } else {
+        next(&cur_process_char, read_buffer);
+    }
+}
+
+
+void turn_red_light_on() {
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_SET);
+}
+
+void turn_yellow_light_on() {
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
+}
+
+void turn_red_and_yellow_lights_off() {
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_14, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_15, GPIO_PIN_RESET);
+}
+
+void turn_green_light_on() {
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_SET);
+}
+
+void turn_green_light_off() {
+    HAL_GPIO_WritePin(GPIOD, GPIO_PIN_13, GPIO_PIN_RESET);
+}
+
+uint32_t get_passed_time(uint32_t startLoopTime) {
+    return HAL_GetTick() - startLoopTime;
+}
+
+uint8_t get_BTN() {
+    return HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_15);
+}
+
 /* USER CODE END 0 */
 
 /**
   * @brief  The application entry point.
   * @retval int
   */
-int main(void)
-{
-  /* USER CODE BEGIN 1 */
+int main(void) {
+    /* USER CODE BEGIN 1 */
 
-  /* USER CODE END 1 */
+    /* USER CODE END 1 */
 
-  /* MCU Configuration--------------------------------------------------------*/
+    /* MCU Configuration--------------------------------------------------------*/
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+    HAL_Init();
 
-  /* USER CODE BEGIN Init */
+    /* USER CODE BEGIN Init */
 
-  /* USER CODE END Init */
+    /* USER CODE END Init */
 
-  /* Configure the system clock */
-  SystemClock_Config();
+    /* Configure the system clock */
+    SystemClock_Config();
 
-  /* USER CODE BEGIN SysInit */
+    /* USER CODE BEGIN SysInit */
 
-  /* USER CODE END SysInit */
+    /* USER CODE END SysInit */
 
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_USART6_UART_Init();
-  /* USER CODE BEGIN 2 */
+    /* Initialize all configured peripherals */
+    MX_GPIO_Init();
+    MX_USART6_UART_Init();
+    /* USER CODE BEGIN 2 */
 
-  /* USER CODE END 2 */
+    duration_for_red = 4 * duration;
+    if (interrupts_mode == 1) {
+        HAL_UART_Receive_IT(&huart6, (uint8_t *) cur_read_char, sizeof(char));
+    }
+    /* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    /* USER CODE END WHILE */
+    /* Infinite loop */
+    /* USER CODE BEGIN WHILE */
+    while (1) {
+        //HAL_UART_Receive_IT(&huart6, (uint8_t *) s, sizeof( char ));
+        //HAL_UART_Transmit_IT( &huart6, (uint8_t *) s, sizeof( s ));
+        /* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
+        /* USER CODE BEGIN 3 */
+        switch (interrupts_mode) {
+            case 1:
+                if (cur_process_char != cur_read_char) {
+                    process_symbol();
+                }
+                if (is_writing_now == 0) {
+                    if (transmit_from_pointer != write_to_pointer) {
+                        is_writing_now = 1;
+                        HAL_UART_Transmit_IT(&huart6, (uint8_t *) transmit_from_pointer, sizeof(char));
+                    }
+                }
+                break;
+            case 0:
+                status = HAL_UART_Receive(&huart6, (uint8_t *) cur_process_char, sizeof(char), 100);
+                if (status == HAL_OK) {
+                    HAL_UART_Transmit(&huart6, (uint8_t *) cur_process_char, sizeof(char), 10);
+                    process_symbol();
+                }
+                break;
+        }
+
+        switch (current_light) {
+            case GPIO_PIN_15:
+                if (HAL_GPIO_ReadPin(GPIOC, GPIO_PIN_15) == 0 && button_flag == 0) {
+                    duration_for_red = duration;
+                    button_flag = 1;
+                }
+                if ((HAL_GetTick() - start_time) >= duration_for_red) {
+                    current_light = GREEN;
+                    duration_for_red = 4 * duration;
+                    button_flag = 0;
+                    turn_specific_light_on(GREEN);
+                    start_time = HAL_GetTick();
+                }
+                break;
+            case GPIO_PIN_14:
+                if ((HAL_GetTick() - start_time) >= duration_for_yellow) {
+                    current_light = RED;
+                    turn_specific_light_on(RED);
+                    start_time = HAL_GetTick();
+                }
+                break;
+            case GPIO_PIN_13:
+                if ((HAL_GetTick() - start_time) >= duration) {
+                    current_light = BLINKING_GREEN;
+                    turn_all_off();
+                    start_time = HAL_GetTick();
+                }
+                break;
+            case 0:
+                if ((HAL_GetTick() - start_time) >= blink_duration) {
+                    blink_count++;
+                    if (blink_count < 6) {
+                        blink(GREEN);
+                    } else {
+                        blink_count = 0;
+                        current_light = YELLOW;
+                        turn_specific_light_on(YELLOW);
+                    }
+                    start_time = HAL_GetTick();
+                }
+                break;
+            default:
+                current_light = RED;
+                turn_specific_light_on(RED);
+                start_time = HAL_GetTick();
+        }
+    }
+    /* USER CODE END 3 */
 }
 
 /**
   * @brief System Clock Configuration
   * @retval None
   */
-void SystemClock_Config(void)
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+void SystemClock_Config(void) {
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+    /** Configure the main internal regulator output voltage
+    */
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = 15;
-  RCC_OscInitStruct.PLL.PLLN = 216;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 4;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    /** Initializes the RCC Oscillators according to the specified parameters
+    * in the RCC_OscInitTypeDef structure.
+    */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLM = 15;
+    RCC_OscInitStruct.PLL.PLLN = 216;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+    RCC_OscInitStruct.PLL.PLLQ = 4;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+        Error_Handler();
+    }
 
-  /** Activate the Over-Drive mode
-  */
-  if (HAL_PWREx_EnableOverDrive() != HAL_OK)
-  {
-    Error_Handler();
-  }
+    /** Activate the Over-Drive mode
+    */
+    if (HAL_PWREx_EnableOverDrive() != HAL_OK) {
+        Error_Handler();
+    }
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+    /** Initializes the CPU, AHB and APB buses clocks
+    */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK
+                                  | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5) != HAL_OK) {
+        Error_Handler();
+    }
 }
 
 /* USER CODE BEGIN 4 */
+void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart) {
+    if (huart->Instance == huart6.Instance) {
+        if (*cur_read_char == '\r') {
+            cur_read_char = read_buffer;
+        } else {
+            write_char_to_buff(*cur_read_char);
+            next(&cur_read_char, read_buffer);
+        }
+        if (interrupts_mode == 1)
+            HAL_UART_Receive_IT(&huart6, (uint8_t *) cur_read_char, sizeof(char));
+    }
+}
+
+void HAL_UART_TxCpltCallback(UART_HandleTypeDef *huart) {
+    if (huart->Instance == huart6.Instance) {
+        is_writing_now = 0;
+        next(&transmit_from_pointer, write_buffer);
+    }
+}
 
 /* USER CODE END 4 */
 
@@ -161,15 +494,13 @@ void SystemClock_Config(void)
   * @brief  This function is executed in case of error occurrence.
   * @retval None
   */
-void Error_Handler(void)
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
+void Error_Handler(void) {
+    /* USER CODE BEGIN Error_Handler_Debug */
+    /* User can add his own implementation to report the HAL error return state */
+    __disable_irq();
+    while (1) {
+    }
+    /* USER CODE END Error_Handler_Debug */
 }
 
 #ifdef  USE_FULL_ASSERT
